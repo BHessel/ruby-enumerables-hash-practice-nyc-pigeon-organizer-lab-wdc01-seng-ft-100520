0ff78116bdd4c2262c7048e9ef2cf9dfaf41450d
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-18 18:05:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,10 +2,8 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   final_results = data.each_with_object ({}) do |(keys, value), final_arr|
-    #binding.pry  
     value.each do |in_key, names|
       names.each do |name|
-      #  binding.pry
         if !final_arr[name]
           final_arr[name] = {}
         end
@@ -16,6 +14,5 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
-  #binding.pry 
 end
  
